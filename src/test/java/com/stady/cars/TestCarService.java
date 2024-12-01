9ffe5c4f2e7804645458471c7ab51f0e54b70746
pinigor1974 <pinigor1974@gmail.com>
2024-12-01 19:04:49 +0300

    TRG-111: fix

--- src/test/java/com/stady/cars/TestCarService.java
+++ src/test/java/com/stady/cars/TestCarService.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 public class TestCarService {
@@ -19,11 +21,98 @@ public class TestCarService {
 
     @Test
     public void testSendCarCode() {
-       Assertions.assertEquals(sendByService.send(Codes.engineOff, carApplicationConfiguration.getCars().get(Cars.LINKOLN.name())), Codes.engineOff);
-       Assertions.assertEquals(sendByService.send(Codes.engineOff, carApplicationConfiguration.getCars().get(Cars.LADA.name())), Codes.error);
+       assertEquals(sendByService.send(Codes.engineOff, carApplicationConfiguration.getCars().get(Cars.LINKOLN.name())), Codes.engineOff);
+       assertEquals(sendByService.send(Codes.engineOff, carApplicationConfiguration.getCars().get(Cars.LADA.name())), Codes.error);
 
         Assertions.assertThrows(NumberFormatException.class, () -> {
             sendByService.send(Codes.engineOn, carApplicationConfiguration.getCars().get(Cars.LADA.name()));
         }, "NumberFormatException error was expected");
     }
+    @Test
+    void maxTreeDepth(){
+        interface TTree {
+            TTree left();
+            TTree right();
+            Integer vle();
+            Boolean isEmpty();
+
+        }
+
+        class Tree implements TTree{
+            public  Integer v;
+            public TTree left;
+            public  TTree right;
+
+            @Override
+            public Boolean isEmpty()  {
+                return false;
+            }
+
+            @Override
+            public TTree left() {
+                return left;
+            }
+
+            @Override
+            public TTree right() {
+                return right;
+            }
+
+            @Override
+            public Integer vle() {
+                return v;
+            }
+
+            public Tree(Integer v, TTree left, TTree right){
+                this.left = left;
+                this.right = right;
+                this.v = v;
+
+            }
+            static Integer findMaxDepth(TTree  tree, Integer acc ) {
+
+                if(tree.isEmpty()) {
+                    return acc;
+                }else{
+                    return Math.max(findMaxDepth(tree.left(), acc + 1), findMaxDepth(tree.left(), acc + 2));
+                }
+
+            }
+
+        };
+
+        final class Nill implements TTree{
+            @Override
+            public TTree left() {
+                return null;
+            }
+
+            @Override
+            public TTree right() {
+                return null;
+            }
+
+            @Override
+            public Integer vle() {
+                return 0;
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return getClass() == obj.getClass();
+            }
+            @Override
+            public Boolean isEmpty()  {
+                return true;
+            }
+        };
+
+
+        var tree = new Tree(1, new Nill(), new Tree(2, new Nill(), new Nill()));
+        assertEquals (Tree.findMaxDepth(tree, 0),2);
+
+    }
+
+
+
 }
